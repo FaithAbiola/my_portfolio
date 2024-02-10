66fddb263a62cce4cedf5a4037cc6f28907b726f
FaithAbiola <abiolafaith92@gmail.com>
2024-02-10 18:06:15 +0100

    App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Intro from "./components/intro/Intro"
 import Portfolio from "./components/portfolio/Portfolio"
 import Works from "./components/works/Works"
 import Contact from "./components/contact/Contact"
-// import "./app.scss"
+import "./App.scss"
 import { useState } from "react";
 import Menu from "./components/menu/Menu";
 
